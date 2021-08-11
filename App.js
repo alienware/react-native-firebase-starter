92100ff37f60f4c86d8f5a621a80cfcc82f1607b
alienware <tanayupadhyaya@gmail.com>
2021-08-11 14:37:48 +0530

    Demo code for google social auth and hitting google cloud function

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+  useState,
+} from 'react';
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import functions from '@react-native-firebase/functions';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId: '1006122924607-nq48ddks8oba9585914ga6cr2g2dlg45.apps.googleusercontent.com',
+});
+
 
 export default function App() {
+  let [name, setName] = useState('');
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  async function onGoogleButtonPress() {
+    // Get the users ID token
+    const { idToken, user } = await GoogleSignin.signIn();
+    let response = await fetch(
+      'https://us-central1-capable-passage-284311.cloudfunctions.net/koinearth', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'message': user})
+      }
+    )
+    let data = await response.json();
+    setName(data['name']);
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <GoogleSigninButton
+        style={{ width: 288, height: 72 }}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={() => onGoogleButtonPress().then(() => Alert.alert(`You are signed as ${name}!`))}
+      />
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,12 @@
+# React Native Firebase Starter
+
+## Getting Started
+
+- Follow https://rnfirebase.io/ for addition of NPM packages and Firebase setup including google-services.json and debug SHA1 key generation
+
+```bash
+yarn start
+yarn android
+```
+
+
